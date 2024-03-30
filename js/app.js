9c793704ea92208e1cef080fd8b1168e6fa4fb21
Simone0730 <simone0730@hotmail.com>
2024-03-30 09:54:03 +0100

    esercizio corretto

--- js/app.js
+++ js/app.js
@@ -10,11 +10,15 @@ const ageInputElement = document.getElementById('age')
 console.log (ageInputElement);
 
 //Calcolare il prezzo totale
-const prezzoTot = parseFloat(0.21 * km) 
+const prezzoTot = parseFloat(0.21 * kmInputElement.value) 
 
 const resultElement = document.getElementById('result')
-let discount = 0.2
-const prezzoFin = parseFloat(prezzoTot - prezzoTot * discount);
+let discountUnder = 0.2
+let discountOver = 0.4
+let noDiscount = 0
+const prezzoFinUn = (prezzoTot - prezzoTot * discountUnder);
+const prezzoFinOv = (prezzoTot - prezzoTot * discountOver);
+const prezzoFin = (prezzoTot - prezzoTot * noDiscount);
 
 
 buttonElement.addEventListener('click', function() {
@@ -22,12 +26,18 @@ buttonElement.addEventListener('click', function() {
     
     const km = parseFloat(kmInputElement.value)
     const age = parseInt(ageInputElement.value)
-    let discount = 0.2
-    const prezzoFin = parseFloat(prezzoTot - prezzoTot * discount);
+    const prezzoTot = parseFloat(0.21 * kmInputElement.value)
+    let discountUnder = 0.2
+    let discountOver = 0.4
+    let noDiscount = 0
+    const prezzoFinUn = (prezzoTot - prezzoTot * discountUnder);
+    const prezzoFinOv = (prezzoTot - prezzoTot * discountOver);
+    const prezzoFin = (prezzoTot - prezzoTot * noDiscount);
+    
 
     //SE si ha meno di 18 anni si avrà uno sconto del 20%
 if(age < 18){
-    let discount = 0.2
+    let discountUnder = 0.2
     resultElement.innerHTML = `
     <table>
     <thead>
@@ -39,17 +49,19 @@ if(age < 18){
     </thead>
     <tbody>
         <tr>
-            <td>${prezzoTot} </td>
+            <td>${prezzoTot + '&euro;'} </td>
             <td>20% di sconto</td>
-            <td>${prezzoFin} </td>
+            <td>${prezzoFinUn.toFixed(2) + '&euro;'} </td>
         </tr>
     </tbody>
    </table>
     `
+    console.log(prezzoTot , prezzoFinUn.toFixed(2))
 }
+
 //ALTRIMENTI SE si ha più di 65 anni si avrà uno sconto del 40%
 else if(age > 65){
-    discount = 0.4
+    let discountOver = 0.4
     resultElement.innerHTML = `
     <table>
     <thead>
@@ -61,17 +73,18 @@ else if(age > 65){
     </thead>
     <tbody>
         <tr>
-            <td>${prezzoTot} </td>
+            <td>${prezzoTot + '&euro;'} </td>
             <td>40% di sconto</td>
-            <td>${prezzoFin} </td>
+            <td>${prezzoFinOv.toFixed(2) + '&euro;'} </td>
         </tr>
     </tbody>
    </table>
     `
+    console.log(prezzoTot , prezzoFinOv.toFixed(2))
  }
  //ALTRIMENTI il prezzo rimarrà quello totale
  else{
-    discount = 0
+    let noDiscount = 0
     resultElement.innerHTML = `
     <table>
     <thead>
@@ -83,13 +96,14 @@ else if(age > 65){
     </thead>
     <tbody>
         <tr>
-            <td>${prezzoTot} </td>
-            <td>/</td>
-            <td>${prezzoFin} </td>
+            <td>${prezzoTot + '&euro;'} </td>
+            <td>Nessuno sconto</td>
+            <td>${prezzoFin + '&euro;'} </td>
         </tr>
     </tbody>
    </table>
     `
+    console.log(prezzoTot , prezzoFin)
  }
 
 })
